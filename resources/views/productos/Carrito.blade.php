32e43748593d61551d65a3e051934c6ee549b2b1
Huracan311 <huracan311@gmail.com>
2019-08-15 23:03:22 -0500

    Correccion tabla carrito

--- resources/views/productos/Carrito.blade.php
+++ resources/views/productos/Carrito.blade.php
@@ -21,9 +21,7 @@
                 <th scope="col">Imágen</th>
                 <th scope="col">Nombre</th>
                 <th scope="col">Precio</th>
-                <th scope="col">Stock</th>
-                <th scope="col"></th>
-                <th class= "" scope="col"></th>
+
             </tr>
             </thead>
             <tbody>
@@ -32,22 +30,6 @@
                     <td><img src="imagenes/{{$carrito->imagen}}" alt="" width="100" height="100"></td>
                     <td>{{$carrito->nombre}}</td>
                     <td>${{number_format($carrito->precio, 0, '.', ',')}} COP</td>
-
-                    @if ($carrito->stock == 1)
-                        <td>{{$carrito->stock}} Unidad</td>
-                    @elseif ($carrito->stock == 0)
-                        <td>Sin unidades</td>
-                    @else
-                        <td>{{$carrito->stock}} Unidades</td>
-                    @endif
-
-                    <td><a href="/productos/{{$carrito->slug}}/edit" class="btn btn-primary">Editar producto</a></td>
-
-                    {{-- <td>
-                        {!! Form::open(['route'=> ['productos.destroy', $producto->slug],'method'=> 'DELETE']) !!}
-                            {!! Form::submit('Eliminar producto', ['class' => 'btn btn-danger'])!!}
-                        {!! Form::close()!!}
-                    </td> --}}
 
             @endforeach
             </tbody>
